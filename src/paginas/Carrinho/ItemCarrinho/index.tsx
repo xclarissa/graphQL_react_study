e82d9ffb9f22038bdacb9a52dd43f7946343928c
Clarissa Xavier <clarissaxr@gmail.com>
2024-04-29 17:17:50 -0300

    creating Carrinho layout and styles

--- src/paginas/Carrinho/ItemCarrinho/index.tsx
+++ src/paginas/Carrinho/ItemCarrinho/index.tsx
@@ -1,11 +1,40 @@
+import { AbInputQuantidade } from "ds-alurabooks";
+import IconeLixeira from "../ItemCarrinho/assets/lixeira.png";
 import { IItemCarrinho } from "../../../interfaces/ICarrinho";
+import { formatador } from "../../../utils/formatador-moeda";
+import "./ItemCarrinho.css";
 
 interface ItemCarrinhoProps {
   item: IItemCarrinho;
 }
 
 const ItemCarrinho = ({ item }: ItemCarrinhoProps) => {
-  return <section>{item.livro.titulo}</section>;
+  return (
+    <section className="itemContainer">
+      <div className="imagem">
+        <img src={item.livro.imagemCapa} alt={item.livro.titulo} height={200} />
+      </div>
+
+      <div className="info">
+        <h3>{item.livro.titulo}</h3>
+        <p>{item.livro.descricao}</p>
+        <p>{item.livro.autor.nome}</p>
+      </div>
+
+      <div>
+        <h3>Preço</h3>
+        <p>{formatador.format(item.opcaoCompra.preco)}</p>
+      </div>
+      
+      <div className="quantidade">
+        <AbInputQuantidade onChange={() => null} value={0} />
+      </div>
+
+      <div className="icon">
+        <img src={IconeLixeira} alt="Ícone lixeira" />
+      </div>
+    </section>
+  );
 };
 
 export default ItemCarrinho;
--- src/paginas/Carrinho/index.tsx
+++ src/paginas/Carrinho/index.tsx
@@ -1,16 +1,22 @@
+import TituloPrincipal from "../../componentes/TituloPrincipal";
 import { useCarrinho } from "../../graphql/carrinho/hooks";
 import ItemCarrinho from "./ItemCarrinho";
+import "./Carrinho.css";
 
 const Carrinho = () => {
   const { data } = useCarrinho();
 
-  return(
-    <section>
-      {data?.carrinho.itens.map((itemCarrinho, index) => (
-        <ItemCarrinho key={index} item={itemCarrinho} />
-      ))}
+  return (
+    <section className="containerItemCarrinho">
+      <TituloPrincipal texto="Minha sacola" />
+      <div className="itens">
+        <h3>Itens selecionados</h3>
+        {data?.carrinho.itens.map((itemCarrinho, index) => (
+          <ItemCarrinho key={index} item={itemCarrinho} />
+        ))}
+      </div>
     </section>
-  )
+  );
 };
 
 export default Carrinho;
